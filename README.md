6b8d76e15a62cae5c145af3a0f9e0351176d7a2c
Awele123 <nwabuzorawele42@gmail.com>
2024-08-08 02:02:36 +0100

    Integrated API

--- README.md
+++ README.md
@@ -9,23 +9,3 @@ padding: 10px;
 display: block;
 margin: 0 auto;
 }
-<div class="humid">
-<ul>
-<li>
-<img
-                  src="https://cdn-icons-png.flaticon.com/512/4907/4907228.png"
-                  alt="Humidity image"
-                />
-</li>
-<li class="humidvalue">45&deg;C</li>
-</ul>
-</div>
-
-           <div class="feels">
-            <ul>
-              <li>
-                <img src="./thermometer.png" alt="Thermometer" />
-              </li>
-              <li>67&deg;C</li>
-            </ul>
-          </div>
--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-function search(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+let nowday = document.querySelector("#day");
+let month = document.querySelector("#month");
+let newday = document.querySelector("#newday");
+let year = document.querySelector("#year")
+let currentDate = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+    "January",
+    "February","March","April","May","June","July","August","September","October","November","December"
+
+]
+day.innerHTML = days[currentDate.getDay()];
+newday.innerHTML= currentDate.getDay();
+month.innerHTML = months[currentDate.getMonth()]; 
+year.innerHTML = currentDate.getFullYear();
+
+
+function refreshWeather(response) {
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    cityElement.innerHTML = response.data.city;
+    let temperatureElement = document.querySelector("#temp");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let humidity = document.querySelector("#humid");
+    humidity.innerHTML = response.data.temperature.humidity +"%";
+    let pressure = document.querySelector("#pressure");
+    pressure.innerHTML = response.data.temperature.pressure +"hPa";
+
 }
+function refreshCondition(response){
+    let condition = document.querySelector("#description");
+    condition.innerHTML = response.data.condition.description;
+    
+}
+
+function searchCity(city) {
+    let apiKey = "afb1fco0e30530debtd23f53ac7d4cbb"
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(refreshWeather);
+    axios.get(apiURL).then(refreshCondition);
+}
+    function search(event) {
+        event.preventDefault();
+        let searchInput = document.querySelector("#search-form-input");
+        searchCity(searchInput.value);
+    }
+
 let searchform = document.querySelector("#search-form");
 searchform.addEventListener("submit", search);
 
